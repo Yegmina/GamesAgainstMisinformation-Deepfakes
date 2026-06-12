84abcbe871e205af9533737385e54fc7fc4e2eb4
Agnessa Tund <agnessa.tund@gmail.com>
2026-06-12 18:57:25 +0300

    feat: add timer, scoring system, and paranoia progress bar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,8 +25,8 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
