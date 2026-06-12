6f536c8749a70afefc55fedb265c6683aacb7f17
Agnessa Tund <agnessa.tund@gmail.com>
2026-06-12 19:34:54 +0300

    feat: add start game scene

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private string interactionText = "Press E to use computer";
+    [SerializeField] private string sceneToLoad = "";
 
     public string InteractionText => interactionText;
 
     public void Interact() // adding actions to the interactable object
     {
         Debug.Log($"Interact with {gameObject.name}");
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
