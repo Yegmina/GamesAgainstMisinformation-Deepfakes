a4094ace3e331e7ed07c5867c11f8e313ad7b520
Agnessa Tund <agnessa.tund@gmail.com>
2026-06-25 08:16:16 +0300

    feat: implement persistent movement hint system

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,24 +7,60 @@ public class UIController : MonoBehaviour
 {
     [Header("UI Elements")]
     [SerializeField] private TextMeshProUGUI textField;
+    [SerializeField] private TextMeshProUGUI movementHintField;
 
     private Animator textAnimator;
     
     private void Awake()
     {
-        textAnimator = textField.GetComponent<Animator>();
+        if (textField != null)
+        {
+            textAnimator = textField.GetComponent<Animator>();
+            textField.text = "";
+        }
 
+        if (movementHintField != null)
+        {
+            movementHintField.text = "WASD to move";
+        }
     }
 
     public void ShowInteraction(string text)
     {
-        textField.text = text;
-        textAnimator.SetBool("Fade", true);
+        if (textField != null)
+        {
+            textField.text = text;
+            if (textAnimator != null) textAnimator.SetBool("Fade", true);
+        }
     }
 
     public void HideInteraction()
     {
-        textAnimator.SetBool("Fade", false);
+        if (textAnimator != null) textAnimator.SetBool("Fade", false);
+    }
+
+    public void ShowMovementHint(string text)
+    {
+        if (GlobalCanvasPersistent.Instance != null)
+        {
+            GlobalCanvasPersistent.Instance.SetMovementHint(text);
+        }
+        else if (movementHintField != null)
+        {
+            movementHintField.text = text;
+        }
+    }
+
+    public void HideMovementHint()
+    {
+        if (GlobalCanvasPersistent.Instance != null)
+        {
+            GlobalCanvasPersistent.Instance.SetMovementHint("");
+        }
+        else if (movementHintField != null)
+        {
+            movementHintField.text = "";
+        }
     }
 
     public void LockCursor()
